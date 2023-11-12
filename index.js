3f44405e1cf0df5b8ed3ae85717006408c681475
Nidhi H N <nidhinshetty28@gmail.com>
2023-11-12 17:38:29 +0530

    Added background image

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var header=document.getElementsByTagName("header");
+/*var header=document.getElementsByTagName("header");
 window.onscroll=function() {scrollFunction()};
 
 function scrollFunction(){
@@ -8,4 +8,5 @@ function scrollFunction(){
     else{
        header[0].removeAttribute("class");
     }
-}
+}*/
+alert(hai);
